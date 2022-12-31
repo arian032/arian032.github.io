210007d3fc1f4a1bb9e708b80a2961691ddae7f6
arian <arianroben7@gmail.com>
2022-12-31 23:13:53 +0330

    add view and url for page

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='home')
+    path('', views.HomePageView.as_view(), name='home'),
+    path('about_us/', views.AboutUsView.as_view(), name='about_us')
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,7 @@ from django.views import generic
 
 class HomePageView(generic.TemplateView):
     template_name = 'page/home.html'
+
+
+class AboutUsView(generic.TemplateView):
+    template_name = 'page/about_us.html'
