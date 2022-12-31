eb0b23f9397a6429a1fa970e9e3fcb84cebbda3c
arian <arianroben7@gmail.com>
2022-12-31 22:50:39 +0330

    setting

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://arian032.github.io/']
 
 
 # Application definition
